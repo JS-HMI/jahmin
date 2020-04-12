77cfc5314397204b83299ef7bdb4725d425bdc1a
panManfredini <motherfucker@motherfucker.com>
2020-04-12 18:50:32 +0200

    jest: JSDOM doesn't support custom-element, happy-dom not support Proxy, cannot use

--- test/Engine.test.js
+++ test/Engine.test.js
@@ -1,10 +1,30 @@
- // import {jashmi} from '../build/main.js'
+import {JsonPollEngine, dataManager} from '../dist/jashmi.js'
+import {LitElement} from 'lit-element'
 
-export default function(){
-    let engine = new jashmi.JsonPollEngine("test_sys")
-    jashmi.dataManager.AddEngine("test_sys",engine);
+test('start', async ()=>{
+    let engine = new JsonPollEngine("test_sys")
+    dataManager.AddEngine("test_sys",engine);
 
     for(let i=0; i<10; i++){
-        jashmi.dataManager.Subscribe({ system:"test_sys", name:"vvv"+i})
+        dataManager.Subscribe({ system:"test_sys", name:"vvv"+i})
     }
-}
+    await new Promise(resolve => setTimeout(resolve, 100));
+    expect(engine.sub_cache.size).toBe(0);
+    expect(Object.keys(engine.subs_count).length).toBe(10);
+    expect(localStorage.getItem("datatree")).not.toBeNull();
+    console.log(localStorage.getItem("datatree"));
+
+    class t extends LitElement{
+        connectedCallback(){
+            super.connectedCallback();
+            console.log("connected");
+        }
+    }
+    window.customElements.define("x-t",t);
+
+    let el = document.createElement("x-t");
+    document.body.append(el);
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+})
+
